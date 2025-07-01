b9bee2f05697daec88259f78abf1dcff147159ea
Nigel Bosch <pnigelb@gmail.com>
2025-07-01 11:34:34 -0500

    add current weather data collection

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 This is a very simply program to continually request departure data from <https://developer.mtd.org/documentation/v2.2/method/getdeparturesbystop/> for a set of stops, and save the results to a file in JSONL format.
 
+It also optionally collects current weather data using the [Open-Meteo API](https://open-meteo.com/en/docs?hourly=&current=temperature_2m#current_weather) if the MTDDATA_WEATHER_OUTPUT_PATH environment variable is set. No API key is required for weather.
+
 ## Setup
 
 Settings are controlled via environment variables, with a `.env` file if you like. View `.env.example` to see the settings.
--- main.go
+++ main.go
@@ -27,9 +27,25 @@ func fetchDepartures(stopID string) ([]byte, error) {
 	return io.ReadAll(resp.Body)
 }
 
-func saveToJSONL(data []byte) error {
-	file, err := os.OpenFile(os.Getenv("MTDDATA_OUTPUT_PATH"),
-		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+func fetchWeather() ([]byte, error) {
+	resp, err := http.Get("https://api.open-meteo.com/v1/forecast?" +
+		"latitude=40.1166557&longitude=88.2297261" +
+		"&current=temperature_2m,relative_humidity_2m,apparent_temperature," +
+		"wind_speed_10m,wind_gusts_10m,precipitation,rain,cloud_cover," +
+		"surface_pressure,snowfall,showers,weather_code,pressure_msl")
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("Weather API request failed with status code: %d",
+			resp.StatusCode)
+	}
+	return io.ReadAll(resp.Body)
+}
+
+func saveToJSONL(data []byte, filepath string) error {
+	file, err := os.OpenFile(filepath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		return err
 	}
@@ -55,12 +71,18 @@ func main() {
 	if os.Getenv("MTDDATA_UPDATE_INTERVAL_SECONDS") == "" {
 		log.Fatal("MTDDATA_UPDATE_INTERVAL_SECONDS is not set in the environment")
 	}
+	weatherPath := os.Getenv("MTDDATA_WEATHER_OUTPUT_PATH")
+	if weatherPath == "" {
+		log.Println("MTDDATA_WEATHER_OUTPUT_PATH not set, so weather will not be saved")
+	}
+
 	// Fetch data regularly, forever
 	updateInterval, err := time.ParseDuration(
 		os.Getenv("MTDDATA_UPDATE_INTERVAL_SECONDS") + "s")
 	if err != nil {
 		log.Fatal("Failed to parse MTDDATA_UPDATE_INTERVAL_SECONDS as a duration:", err)
 	}
+	log.Println("Starting; data will not be fetched until first update interval")
 	ticker := time.NewTicker(updateInterval)
 	defer ticker.Stop()
 	for range ticker.C {
@@ -71,9 +93,17 @@ func main() {
 				log.Print("Failed to fetch departures for stop", stopID, ":", err)
 				continue
 			}
-			if err := saveToJSONL(data); err != nil {
+			if err := saveToJSONL(data, os.Getenv("MTDDATA_OUTPUT_PATH")); err != nil {
 				log.Print("Failed to save departures data for stop", stopID, ":", err)
 				continue
+			}
+		}
+		if weatherPath != "" {
+			data, err := fetchWeather()
+			if err != nil {
+				log.Print("Failed to fetch weather:", err)
+			} else if err := saveToJSONL(data, weatherPath); err != nil {
+				log.Print("Failed to save weather data:", err)
 			}
 		}
 		log.Println("Fetched and saved departures data for", len(stopIDs), "stops")
